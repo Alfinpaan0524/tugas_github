0dc2e4ab865e2ad566cf283399506afea47509b6
Alfinpaan0524 <alfin.alfinputraa53@gmail.com>
2020-09-13 20:34:56 +0700

    first commit

---
+++ app/Http/Controllers/TesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesController extends Controller
+{
+    public function show(){
+        return "Tes Controller Berhasil";
+    }
+
+    public function insert(){
+
+    }
+
+    public function delete(){
+        
+    }
+}
